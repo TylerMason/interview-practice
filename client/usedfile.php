d60b59894ba19698af2f054ae7719ae83db9b900
Tyler Mason <tymasn@gmail.com>
2020-02-21 10:07:22 -0700

    messing with database connection using php

--- client/usedfile.php
+++ client/usedfile.php
@@ -1,5 +1,22 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "dummy pass";
 
-    $db = "Yo, ";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
 ?>
